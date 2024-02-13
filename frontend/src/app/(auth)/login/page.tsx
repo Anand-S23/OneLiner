dbac44688c7c7b49345f240aa7873f0cb48b855d
Anand-S23 <anandsroopsingh23@gmail.com>
2024-02-13 13:06:20 -0500

    Styling Login and Regsiter pages

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -2,15 +2,32 @@
 
 import LoginForm from "@/components/auth/LoginForm";
 import StaticNavbar from "@/components/ui/static-navbar";
+import { useRouter } from "next/navigation";
 
 function Login() {
+    const router = useRouter();
+
     return (
         <>
             <StaticNavbar
                 text="Register"
                 redirect="/register"
             />
-            <LoginForm />
+
+            <div className="flex justify-center items-center h-full py-12">
+                <div className="bg-white p-8 rounded shadow-lg w-96">
+                    <h1 className="px-5 text-2xl text-semibold pt-5"> Login </h1>
+
+                    <LoginForm />
+
+                    <p className="px-5">
+                        New User? <span 
+                            className="text-blue-500 hover:text-blue-400 hover:cursor-pointer hover:underline"
+                            onClick={() => router.push('/register')}
+                        >Register</span>
+                    </p> 
+                </div>
+            </div>
         </>
     );
 }
--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -2,8 +2,11 @@
 
 import RegisterForm from "@/components/auth/RegisterForm";
 import StaticNavbar from "@/components/ui/static-navbar";
+import { useRouter } from "next/navigation";
 
 function Register() {
+    const router = useRouter();
+
     return (
         <>
             <StaticNavbar
@@ -11,7 +14,20 @@ function Register() {
                 redirect="/login"
             />
 
-            <RegisterForm />
+            <div className="flex justify-center items-center h-full py-12">
+                <div className="bg-white p-8 rounded shadow-lg w-96">
+                    <h1 className="px-5 text-2xl text-semibold pt-5"> Register </h1>
+
+                    <RegisterForm />
+
+                    <p className="px-5">
+                        Already a User? <span 
+                            className="text-blue-500 hover:text-blue-400 hover:cursor-pointer hover:underline"
+                            onClick={() => router.push('/login')}
+                        >Login</span>
+                    </p> 
+                </div>
+            </div>
         </>
     );
 }
